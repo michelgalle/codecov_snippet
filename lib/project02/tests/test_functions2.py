a918794e64c5240067d5fb069196b99b77937bdb
Michel Galle <michel.galle@cherre.com>
2021-08-04 17:28:02 -0300

    test

--- lib/project02/tests/test_functions2.py
+++ lib/project02/tests/test_functions2.py
@@ -1,7 +1,7 @@
 from project02 import Functions2
 
 def test_main():
-    assert Functions2.main() == "hello world"
+    assert Functions2.main() == "hello world 2"
 
 def test_version():
-    assert Functions2.version() == "version 1.0"
+    assert Functions2.version() == "version 1.0 project 2"
