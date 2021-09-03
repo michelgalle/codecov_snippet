8193b1242a5fff3e31260ada711dee7c9e488b7a
Michel Galle <michel.galle@cherre.com>
2021-09-03 11:26:10 -0300

    test

--- lib/project02/project02/functions2.py
+++ lib/project02/project02/functions2.py
@@ -10,6 +10,11 @@ class Functions2:
     def author():
         return "author project 2"
 
+    def do_something():
+        print("this is doing something")
+        print("it is testing the carry forward flags")
+        return True
+
     if __name__ == '__main__':
         print(main())
 
